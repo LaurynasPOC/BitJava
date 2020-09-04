1de8cad32c2a545082f1b504ccd57722d3a32a7c
LaurynasPO <laurynas.pociunas@gmail.com>
2020-09-04 14:46:08 +0300

    Adding first commit

---
+++ src/lt/laurynas/app/HelloWorld.java
@@ -0,0 +1,16 @@
+package lt.laurynas.app;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("labas vakaras lietuva");
+        String name = "Jonh Gahret";
+        System.out.println(name);
+        int num = 20;
+        num = 30;
+        System.out.println(num);
+
+    }
+}
+
+
